58c73fa161f3fdfff8d832c6adfc52b5941ae160
juans <juansmgb@gmail.com>
2022-07-05 16:40:41 -0500

    quito commit

--- src/components/technologies.js
+++ src/components/technologies.js
@@ -11,46 +11,39 @@ const Technologies = () => {
 
     return(
         <div className="tecno">
-            
- 
-
+        
             <div className="tecno--content">
             <FontAwesomeIcon className="icon--tecno" icon={faHtml5}/>
             <h3 className="tecno--subtitle">Html</h3>
             </div>
 
+
             <div className="tecno--content">
             <FontAwesomeIcon className="icon--tecno" icon={faCss3Alt}/>
             <h3 className="tecno--subtitle">Css</h3>
-            
             </div>
 
             <div className="tecno--content">
             <FontAwesomeIcon className="icon--tecno" icon={faJsSquare}/>
             <h3 className="tecno--subtitle">JavaScritp</h3>
-
             </div>
 
             <div className="tecno--content">
             <FontAwesomeIcon className="icon--tecno" icon={faReact}/>
             <h3 className="tecno--subtitle">React</h3>
-       
             </div>
 
 
             <div className="tecno--content">
             <FontAwesomeIcon className="icon--tecno" icon={faNodeJs}/>
             <h3 className="tecno--subtitle">Node Js</h3>
-          
             </div>
 
     
             <div className="tecno--content">
             <FontAwesomeIcon className="icon--tecno" icon={faBootstrap}/>
             <h3 className="tecno--subtitle">Bootstrap</h3>
-          
             </div>
-
 
         </div>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -66,18 +66,22 @@ const Index = ({ data, location }) => {
         </section>
 
         <section className="technologies">
-        <svg className="wave--top" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-          <path fill="#0c151d" fill-opacity="1" d="M0,96L120,133.3C240,171,480,245,720,250.7C960,256,1200,192,1320,160L1440,128L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"></path>
-        </svg>
-        <h2 className="title--techno">Tecnologias</h2>
+          <svg className="wave--top" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#0c151d" fill-opacity="1" d="M0,96L120,133.3C240,171,480,245,720,250.7C960,256,1200,192,1320,160L1440,128L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"></path>
+          </svg>
 
-        <section className="tecno--content">
-        <Technologies />
-        </section>
+            <h2 className="title--tecno">Habilidades</h2>
+            <p className="sub--tecno"> 
+              Uso de tecnologías para el desarrollo de aplicaciones y sitios web
+            </p>
+         
+          <section className="tecno--content">
+          <Technologies />
+          </section>
 
-        <svg className="wave--botton" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-          <path fill="#0c151d" fill-opacity="1" d="M0,96L120,133.3C240,171,480,245,720,250.7C960,256,1200,192,1320,160L1440,128L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path>
-        </svg>
+          <svg className="wave--botton" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#0c151d" fill-opacity="1" d="M0,96L120,133.3C240,171,480,245,720,250.7C960,256,1200,192,1320,160L1440,128L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path>
+          </svg>
         </section>
 
       </main>
