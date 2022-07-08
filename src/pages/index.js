8d477b03b15b4f8511ac496ad4543cfa9b7c7d1f
juans <juansmgb@gmail.com>
2022-07-08 01:48:37 -0500

    netlifi delate form

--- src/pages/index.js
+++ src/pages/index.js
@@ -79,7 +79,6 @@ const Index = ({ data, location }) => {
             <Technologies />
           </section>
 
-
           <svg preserveAspectRatio="none" className="wave--botton" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
             <path fill="#0c151d" fill-opacity="1" d="M0,96L120,133.3C240,171,480,245,720,250.7C960,256,1200,192,1320,160L1440,128L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path>
           </svg>
