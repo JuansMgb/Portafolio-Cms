1a192774cf1d2b208c5bf6904412f50401ed25aa
juans <juansmgb@gmail.com>
2025-02-12 16:03:23 -0500

    Archivos estilos

--- src/pages/index.js
+++ src/pages/index.js
@@ -58,7 +58,7 @@ const Index = ({ data, location }) => {
 
         </section>
            
-        <svg preserveAspectRatio="none" className="wavo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <svg preserveAspectRatio="none" className="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
         <path fill="#0c151d" fill-opacity="1" d="M0,96L120,112C240,128,480,160,720,160C960,160,1200,128,1320,112L1440,96L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path>
         </svg>
 
