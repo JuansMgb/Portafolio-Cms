ab15ab23607e48b741ef10af553e84b62becceec
juans <juansmgb@gmail.com>
2025-02-12 16:35:43 -0500

    Archivos estilos

--- src/pages/index.js
+++ src/pages/index.js
@@ -355,12 +355,11 @@ const Index = ({ data, location }) => {
 
         </section>
 
-        <svg preserveAspectRatio="none" className="wave wave-animation" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-          <path fill="#0c151d" fillOpacity="1" d="M0,96L120,112C240,128,480,160,720,160C960,160,1200,128,1320,112L1440,96L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path>
+        <svg preserveAspectRatio="none" className="wave--decoreitor wave-animation" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path fill="#0c151d" fillOpacity="1" stroke="#0c151d" stroke-width="10" d="M0,96L120,112C240,128,480,160,720,160C960,160,1200,128,1320,112L1440,96L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path>
         </svg>
 
-        <section>
-          
+        
         <section className="section--med"></section>
 
           <section className="section--form">
@@ -388,7 +387,7 @@ const Index = ({ data, location }) => {
 
           </section>
       
-        </section>
+        
 
         
 
