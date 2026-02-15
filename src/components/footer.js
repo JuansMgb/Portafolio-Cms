589f00939bb8b2c4b4d165e5e94abd5ea2cbb256
juans <juansmgb@gmail.com>
2026-02-15 10:35:22 -0500

    Actulizacion nav

--- src/components/footer.js
+++ src/components/footer.js
@@ -24,7 +24,7 @@ const Footer = () => {
         <a href="https://www.instagram.com/konchiser/" target={"_blank"}><FontAwesomeIcon className="icon" icon={faInstagram}/></a>
         <a href="https://www.linkedin.com/in/juans-crs-b306bb345/" target={"_blank"}><FontAwesomeIcon className="icon" icon={faLinkedin}/></a>
         <a href="https://github.com/JuansMgb" target={"_blank"}><FontAwesomeIcon className="icon" icon={faGithub}/></a>
-        <a href="https://wa.me/573116309784?text=Hola :D, Dejame un mensaje" target={"_blank"}><FontAwesomeIcon className="icon" icon={faWhatsapp}/></a>
+        <a href="https://wa.me/573015736640?text=Holaa :D, Dejame un mensaje" target={"_blank"}><FontAwesomeIcon className="icon" icon={faWhatsapp}/></a>
       </section>
 
       <section className="footer--texts">
