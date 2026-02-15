c7a1794b5be043c51bca90f491f95f69d2100cb4
juans <juansmgb@gmail.com>
2026-02-15 10:28:28 -0500

    Actulizacion nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -18,7 +18,7 @@ const Nav = () => {
                 <a href="https://www.instagram.com/sebastian_chrc/" target={"_blank"}><FontAwesomeIcon className="icon" icon={faInstagram}/></a>
                 <a href="https://www.linkedin.com/in/juans-crs-b306bb345/" target={"_blank"}><FontAwesomeIcon className="icon" icon={faLinkedin}/></a>
                 <a href="https://github.com/JuansMgb" target={"_blank"}><FontAwesomeIcon className="icon" icon={faGithub}/></a>
-                <a href="https://wa.me/573015736640?text=Hola :D, Dejame un mensaje" target={"_blank"}><FontAwesomeIcon className="icon" icon={faWhatsapp}/></a>
+                <a href="https://wa.me/573015736640?text=Holaa :D, Dejame un mensaje" target={"_blank"}><FontAwesomeIcon className="icon" icon={faWhatsapp}/></a>
             </section>
 
             <svg preserveAspectRatio="none" className="wave wave-animation" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
